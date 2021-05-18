6911ebd5a98d20587daf391b35a862e33b25f63b
Harry <harry@apartalk.com>
2021-05-18 17:38:36 +0900

    feat: jwt_model, entity 생성

--- app/extensions/utils/time_helper.py
+++ app/extensions/utils/time_helper.py
@@ -1,5 +1,15 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+
+from flask import current_app
 
 
 def get_server_timestamp():
     return datetime.now()
+
+
+def get_jwt_access_expired_timestamp():
+    return get_server_timestamp() - timedelta(minutes=30)
+
+
+def get_jwt_refresh_expired_timestamp():
+    return get_server_timestamp() - timedelta(days=14)
