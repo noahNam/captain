5666fd89c0fdcf9d42a02429abe4bda1d2b5d65f
Harry <harry@apartalk.com>
2021-05-26 02:31:35 +0900

    feat: blacklist, update 기능 추가

--- core/domains/authentication/repository/authentication_repository.py
+++ core/domains/authentication/repository/authentication_repository.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Optional
 
 from app import redis
@@ -5,7 +6,10 @@ from app.extensions.database import session
 from app.extensions.utils.log_helper import logger_
 from app.extensions.utils.time_helper import get_jwt_access_expired_timestamp, get_jwt_refresh_expired_timestamp, \
     get_jwt_access_expire_timedelta_to_seconds, get_jwt_refresh_expire_timedelta_to_seconds
+from app.persistence.model import BlacklistModel
 from app.persistence.model.jwt_model import JwtModel
+from core.domains.authentication.dto.authentication_dto import GetBlacklistDto
+from core.domains.authentication.entity.blacklist_entity import BlacklistEntity
 from core.domains.authentication.entity.jwt_entity import JwtEntity
 from core.domains.user.dto.user_dto import GetUserDto
 from flask_jwt_extended import create_access_token, create_refresh_token
@@ -22,13 +26,17 @@ class AuthenticationRepository:
             return True
         return False
 
-    def _update_token(self, dto: GetUserDto, token_info: JwtModel) -> None:
+    def _update_token(self, dto: GetUserDto) -> None:
+        """
+            새로운 Token 생성
+            update 시간 갱신
+        """
         try:
             session.query(JwtModel).filter_by(user_id=dto.user_id) \
                 .update(
                 {
-                    "access_token": token_info.access_token,
-                    "refresh_token": token_info.refresh_token,
+                    "access_token": create_access_token(identity=dto.user_id),
+                    "refresh_token": create_refresh_token(identity=dto.user_id),
                     "access_expired_at": get_jwt_access_expired_timestamp(),
                     "refresh_expired_at": get_jwt_refresh_expired_timestamp()
                 }
@@ -41,7 +49,14 @@ class AuthenticationRepository:
                 f"error : {e}"
             )
 
-    def _create_token(self, dto: GetUserDto, token_info: JwtModel) -> None:
+    def _create_token(self, dto: GetUserDto) -> None:
+
+        token_info = JwtModel(
+            user_id=dto.user_id,
+            access_token=create_access_token(identity=dto.user_id),
+            refresh_token=create_refresh_token(identity=dto.user_id),
+        )
+
         try:
             session.add(token_info)
             session.commit()
@@ -52,22 +67,29 @@ class AuthenticationRepository:
                 f"error : {e}"
             )
 
-    def create_or_update_token(self, dto: GetUserDto) -> Optional[JwtEntity]:
+    def create_or_update_token(self, dto: GetUserDto) -> None:
         """
             case 1: 신규 회원가입 -> Create
             case 2: 기존 유저가 로그아웃 후 재로그인 -> Update
         """
-        token_info = JwtModel(
-            user_id=dto.user_id,
-            access_token=create_access_token(identity=dto.user_id),
-            refresh_token=create_refresh_token(identity=dto.user_id)
-        )
-
         if self._is_exists(dto=dto):
-            self._update_token(dto=dto, token_info=token_info)
+            # 현재 시간 기준 업데이트
+            self._update_token(dto=dto)
         else:
-            self._create_token(dto=dto, token_info=token_info)
+            self._create_token(dto=dto)
 
+    def get_token_info_by_dto(self, dto: GetUserDto) -> Optional[JwtEntity]:
+        token_info = session.query(JwtModel).filter_by(user_id=dto.user_id).first()
+
+        if not token_info:
+            return None
+        return token_info.to_entity()
+
+    def get_token_info_by_user_id(self, user_id: int) -> Optional[JwtEntity]:
+        token_info = session.query(JwtModel).filter_by(user_id=user_id).first()
+
+        if not token_info:
+            return None
         return token_info.to_entity()
 
     def _set_access_token_to_cache(self, token_info: Optional[JwtEntity]):
@@ -97,9 +119,56 @@ class AuthenticationRepository:
             )
 
     def set_token_to_cache(self, token_info: Optional[JwtEntity]) -> bool:
+        """
+            Save Token_info to Redis (key : value)
+            - jwt_access_token : user_id
+            - user_id : jwt_refresh_token
+        """
         if token_info:
             self._set_access_token_to_cache(token_info)
             self._set_refresh_token_to_cache(token_info)
 
             return True
         return False
+
+    def create_blacklist(self, dto: GetBlacklistDto):
+        blacklist = BlacklistModel(user_id=dto.user_id,
+                                   access_token=dto.access_token)
+
+        try:
+            session.add(blacklist)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            logger.error(
+                f"[AuthenticationRepository][create_blacklist] user_id : {dto.user_id}, "
+                f"access_token : {dto.access_token} error : {e}"
+            )
+
+    def get_blacklist_by_dto(self, dto: GetBlacklistDto) -> Optional[BlacklistEntity]:
+        blacklist = session.query(BlacklistModel).filter_by(user_id=dto.user_id).first()
+
+        if not blacklist:
+            return None
+        return blacklist.to_entity()
+
+    def get_blacklist_by_user_id(self, user_id: int) -> Optional[BlacklistEntity]:
+        blacklist = session.query(JwtModel).filter_by(user_id=user_id).first()
+
+        if not blacklist:
+            return None
+        return blacklist.to_entity()
+
+    def delete_blacklist(self, dto: GetBlacklistDto):
+        blacklist = BlacklistModel(user_id=dto.user_id,
+                                   access_token=dto.access_token)
+
+        try:
+            session.delete(blacklist)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            logger.error(
+                f"[AuthenticationRepository][delete_blacklist] user_id : {dto.user_id}, "
+                f"access_token : {dto.access_token} error : {e}"
+            )
