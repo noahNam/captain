a8ab45766c09fa60cfac8317e99debeac5f6531a
Harry <harry@apartalk.com>
2021-05-26 02:25:26 +0900

    feat: create_token pubsub 기능 추가-redis 저장

--- core/domains/authentication/event/__init__.py
+++ core/domains/authentication/event/__init__.py
@@ -7,8 +7,9 @@ from core.domains.user.dto.user_dto import GetUserDto
 
 
 def create_token(dto: GetUserDto) -> None:
-    token_info = AuthenticationRepository().create_or_update_token(dto=dto)
-    # token_info = AuthenticationRepository().set_token_to_cache(dto=dto)
+    AuthenticationRepository().create_or_update_token(dto=dto)
+    token_info = AuthenticationRepository().get_token_info_by_dto(dto=dto)
+    AuthenticationRepository().set_token_to_cache(token_info=token_info)
     setattr(g, AuthenticationTopicEnum.CREATE_OR_UPDATE_TOKEN, token_info)
 
 
