7af3def8abee39076f422780c06d025506b9dafb
Harry <harry@apartalk.com>
2021-05-26 02:23:34 +0900

    feat: time_helper token delta값 반환 추가

--- app/extensions/utils/time_helper.py
+++ app/extensions/utils/time_helper.py
@@ -5,17 +5,25 @@ def get_server_timestamp() -> datetime:
     return datetime.now()
 
 
-def get_jwt_access_expired_timestamp() -> datetime:
-    return get_server_timestamp() + timedelta(minutes=30)
-
-
-def get_jwt_refresh_expired_timestamp() -> datetime:
-    return get_server_timestamp() + timedelta(days=14)
+def get_jwt_access_expired_time_delta() -> timedelta:
+    return timedelta(minutes=30)
 
 
 def get_jwt_access_expire_timedelta_to_seconds() -> int:
     return int(timedelta(minutes=30).total_seconds())
 
 
+def get_jwt_refresh_expired_time_delta() -> timedelta:
+    return timedelta(days=14)
+
+
 def get_jwt_refresh_expire_timedelta_to_seconds() -> int:
     return int(timedelta(days=14).total_seconds())
+
+
+def get_jwt_access_expired_timestamp() -> datetime:
+    return get_server_timestamp() + timedelta(minutes=30)
+
+
+def get_jwt_refresh_expired_timestamp() -> datetime:
+    return get_server_timestamp() + timedelta(days=14)
