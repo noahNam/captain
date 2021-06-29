312ded52b54c7c7059762c45b67ebc1f2c3906c8
Harry <harry@apartalk.com>
2021-06-29 12:35:58 +0900

    style: black 적용

--- app/config.py
+++ app/config.py
@@ -29,7 +29,7 @@ class LocalConfig(Config):
 class TestConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = (
-            os.environ.get("TEST_DATABASE_URL") or "sqlite:///:memory:"
+        os.environ.get("TEST_DATABASE_URL") or "sqlite:///:memory:"
     )
 
     WTF_CSRF_ENABLED = False
