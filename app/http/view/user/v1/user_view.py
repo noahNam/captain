103ecc1f9bbf56498f781dcc81bf8ebfc5ea8f28
Noah Nam <noah@apartalk.com>
2021-07-08 20:33:06 +0900

    tanos에서 provider 조회용 call api 구축

--- app/http/view/user/v1/user_view.py
+++ app/http/view/user/v1/user_view.py
@@ -1,11 +1,11 @@
 from flasgger import swag_from
 from flask_jwt_extended import jwt_required
 
-from app.http.requests.view.user.v1.user_request import GetUserRequest
+from app.http.requests.view.user.v1.user_request import GetUserRequest, GetUserProviderRequest
 from app.http.responses import failure_response
-from app.http.responses.presenters.user_presenter import UserPresenter
+from app.http.responses.presenters.user_presenter import UserPresenter, GetUserProviderPresenter
 from app.http.view import auth_required, api
-from core.domains.user.use_case.v1.user_use_case import GetUserUseCase
+from core.domains.user.use_case.v1.user_use_case import GetUserUseCase, GetUserProviderUseCase
 from core.use_case_output import FailureType, UseCaseFailureOutput
 
 
@@ -20,3 +20,16 @@ def get_user_view(user_id):
         )
 
     return UserPresenter().transform(GetUserUseCase().execute(dto=dto))
+
+
+@api.route("/v1/users/provider", methods=["GET"])
+@jwt_required
+@auth_required
+def get_user_provider_view(user_id):
+    dto = GetUserProviderRequest(user_id=user_id).validate_request_and_make_dto()
+    if not dto:
+        return failure_response(
+            UseCaseFailureOutput(type=FailureType.INVALID_REQUEST_ERROR)
+        )
+
+    return GetUserProviderPresenter().transform(GetUserProviderUseCase().execute(dto=dto))
--- core/domains/user/use_case/v1/user_use_case.py
+++ core/domains/user/use_case/v1/user_use_case.py
@@ -2,20 +2,34 @@ from typing import Union
 
 import inject
 
-from core.domains.user.dto.user_dto import GetUserDto
+from core.domains.user.dto.user_dto import GetUserDto, GetUserProviderDto
 from core.domains.user.repository.user_repository import UserRepository
 from core.use_case_output import UseCaseSuccessOutput, UseCaseFailureOutput, FailureType
 
 
-class GetUserUseCase:
+class UserBaseUseCase:
     @inject.autoparams()
     def __init__(self, user_repo: UserRepository):
-        self.__user_repo = user_repo
+        self._user_repo = user_repo
 
+
+class GetUserUseCase(UserBaseUseCase):
     def execute(
-        self, dto: GetUserDto
+            self, dto: GetUserDto
     ) -> Union[UseCaseSuccessOutput, UseCaseFailureOutput]:
-        user = self.__user_repo.get_user_by_user_id(user_id=dto.user_id)
+        user = self._user_repo.get_user_by_user_id(user_id=dto.user_id)
         if not user:
             return UseCaseFailureOutput(detail=FailureType.NOT_FOUND_ERROR)
         return UseCaseSuccessOutput(value=user)
+
+
+class GetUserProviderUseCase(UserBaseUseCase):
+    def execute(
+            self, dto: GetUserProviderDto
+    ) -> Union[UseCaseSuccessOutput, UseCaseFailureOutput]:
+        if not dto.user_id:
+            return UseCaseFailureOutput(detail=FailureType.NOT_FOUND_ERROR)
+
+        user_provider: str = self._user_repo.get_user_provider(dto=dto)
+
+        return UseCaseSuccessOutput(value=user_provider)
